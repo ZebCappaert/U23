8fd953bdce723c1a706d1de0d7e9dc3338213014
Zeb <zeb.cappaert@gmail.com>
2026-03-04 12:05:14 +0100

    Added basic usercontoller

--- backend/src/main/java/softball/app/AppApplication.java
+++ backend/src/main/java/softball/app/AppApplication.java
@@ -8,7 +8,7 @@ public class AppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
-		System.out.println("Running on http://localhost:8080");
+		System.out.println("Running on http://localhost:8080/api/users/");
 	}
 
 }
---
+++ backend/src/main/java/softball/app/controller/UserController.java
@@ -0,0 +1,28 @@
+package softball.app.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import softball.app.jpa.User;
+import softball.app.repository.UserRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RestController
+@RequestMapping("/api/users")
+@CrossOrigin(origins = "http://localhost:4200")
+public class UserController {
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+}
